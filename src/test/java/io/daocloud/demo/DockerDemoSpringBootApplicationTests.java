e80765435748b7253515bcc061017d9e287464c2
neverland068 <35914183+neverland068@users.noreply.github.com>
2018-01-29 16:54:56 +0800

    springBootTestMemo01

---
+++ src/test/java/io/daocloud/demo/DockerDemoSpringBootApplicationTests.java
@@ -0,0 +1,12 @@
+package io.daocloud.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+public class DockerDemoSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
